5a6d5823b2f9aaf441c8784dc403bc1b0f57b22a
Fields Barnett <fieldsbarnett@gmail.com>
2019-08-26 14:11:12 -0500

    Fixes for prod build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
   email = "";
   password = "";
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4hKeRQVZoXKxOLkdOo62Qfsy8VH-oHUg",
+    authDomain: "when-you-feel.firebaseapp.com",
+    databaseURL: "https://when-you-feel.firebaseio.com",
+    projectId: "when-you-feel",
+    storageBucket: "",
+    messagingSenderId: "429915765272",
+    appId: "1:429915765272:web:af56322fa8b785b2"
+  },
 };
